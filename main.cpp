ec7d1577593a418804838db6180e8df7c2d6cf94
JerzyJancewicz <jancewiczjerzy@gmail.com>
2022-05-10 19:05:04 +0200

    stworzenie klasy main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+/**
+ * co funkcja robi
+ * @param z
+ * @return char // co zwraca
+ */
+char wyswietl(char z){
+    return z;
+}
+
+int main(){
+
+    std::cout << " cos " << std::endl;
+
+    return 0;
+}
